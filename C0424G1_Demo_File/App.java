54288b00f188234255eb865ea9251f78264e7137
huy12345a <Quanghuy.hmtb@gmail.com>
2024-07-01 17:20:50 +0700

    fix loi

--- C0424G1_Demo_File/App.java
+++ C0424G1_Demo_File/App.java
@@ -3,6 +3,15 @@ import src.StudentManager;
 import java.util.Scanner;
 
 public class App {
+
+    public static final int EXIT_CHOICE = 7;
+    public static final int ADD_STUDENT = 1;
+    public static final int SHOW_LIST_STUDENT = 2;
+    public static final int DELETE_STUDENT = 3;
+    public static final int UPDATE_STUDENT = 4;
+    public static final int IMPORT_STUDENT = 5;
+    public static final int EXPORT_STUDENT = 6;
+
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
@@ -17,24 +26,24 @@ public class App {
                 System.out.println("6: Export students");
                 System.out.println("7: Exit");
                 int choice = Integer.parseInt(scanner.nextLine());
-                if (choice == 7) {
+                if (choice == EXIT_CHOICE) {
                     break;
                 }
                 switch (choice) {
-                    case 1:
-                        StudentManager.addStudent();
+                    case ADD_STUDENT:
+                        StudentManager.addStudent(scanner);
                         break;
-                    case 2:
+                    case SHOW_LIST_STUDENT:
                         StudentManager.showListStudent();
                         break;
-                    case 3:
-                        StudentManager.deleteStudent();
-                    case 4:
-                        StudentManager.fixStudent();
-                    case 5:
-                        StudentManager.importStudents();
-                    case 6:
-                        StudentManager.exportStudents();
+                    case DELETE_STUDENT:
+                        StudentManager.deleteStudent(scanner);
+                    case UPDATE_STUDENT:
+                        StudentManager.updateStudent(scanner);
+                    case IMPORT_STUDENT:
+                        StudentManager.importStudents(scanner);
+                    case EXPORT_STUDENT:
+                        StudentManager.exportStudents(scanner);
                 }
             }
 
